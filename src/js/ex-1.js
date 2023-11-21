566125bc7967fd607ed3e680afd7d24532e6eb75
Colunga-D <xkould@gmail.com>
2023-11-21 19:15:14 +0100

    exercises js algoritmos

--- src/js/ex-1.js
+++ src/js/ex-1.js
@@ -1,5 +1,22 @@
 //ejercicio 1
 
+// Definición de Variables:
+
+// Se define una constante llamada title con el valor "Hello Javascript".
+// Se obtiene una referencia al elemento HTML con el id 'title' y se almacena en la constante titleSection.
+// Función printTitle:
+
+// Se define una función llamada printTitle.
+// Dentro de la función:
+// Se actualiza el contenido HTML del elemento con el id 'title' (almacenado en titleSection).
+// El nuevo contenido es un encabezado <h1> que contiene el valor de la constante title.
+// Llamada a la Función:
+
+// Se llama a la función printTitle para ejecutar el código dentro de ella. Esto actualiza el contenido HTML del elemento con el id 'title' en la página.
+// Mostrar en la Consola:
+
+// Se utiliza console.log para imprimir en la consola el mensaje 'conected'.
+
 const title = "Hello Javascript"
 const titleSection = document.getElementById('title')
 
--- src/js/ex-15.js
+++ src/js/ex-15.js
@@ -1,13 +1,21 @@
 // Ejercicio 15
+// Solicitar al usuario que ingrese un número
+// Parsear el número ingresado a un entero
 const numero = parseInt(prompt("Ingresa un número:"));
 
-
+// Inicializar una variable para indicar si el número es primo
+// Un número es considerado primo hasta que se encuentre un divisor diferente de 1 y de sí mismo
 let esPrimo = true;
 
+// Verificar si el número es menor o igual a 1, ya que los números primos son mayores que 1
+// Si es menor o igual a 1, no es primo
 if (numero <= 1) {
   esPrimo = false;
 } else {
+  // Iterar desde 2 hasta el número anterior al ingresado por el usuario
   for (let i = 2; i < numero; i++) {
+    // Verificar si el número es divisible por algún número en el rango
+    // Si es divisible, no es primo, y se rompe el bucle
     if (numero % i === 0) {
       esPrimo = false;
       break;
@@ -15,8 +23,11 @@ if (numero <= 1) {
   }
 }
 
+// Construir un mensaje indicando si el número es primo o no
 const mensaje = esPrimo
   ? `El número ${numero} es primo.`
   : `El número ${numero} no es primo.`;
 
+// Mostrar el resultado en la consola o en la pantalla
+// Imprimir el mensaje
 console.log(mensaje);
